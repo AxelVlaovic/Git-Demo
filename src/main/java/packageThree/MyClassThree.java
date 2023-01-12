daa1d979c5fd551787e92e7aff305d4bd25110ab
AkselVlaovic <avlaovic2619ri@raf.rs>
2023-01-12 02:31:11 +0100

    MyClassThree finished.

--- src/main/java/packageThree/MyClassThree.java
+++ src/main/java/packageThree/MyClassThree.java
@@ -4,6 +4,10 @@ public class MyClassThree {
 
     private Object myObject;
 
+    public MyClassThree(Object myObject) {
+        this.myObject = myObject;
+    }
+
     public Object getMyObject() {
         return myObject;
     }
