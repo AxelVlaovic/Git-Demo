19abe96af25573e21f832051c41f8a439005824b
AkselVlaovic <avlaovic2619ri@raf.rs>
2023-01-12 01:33:44 +0100

    My first commit

---
+++ src/main/java/main/MyClassOne.java
@@ -0,0 +1,17 @@
+package main;
+
+public class MyClassOne {
+
+    private Object myAttribute;
+
+    public Object getMyAttribute() {
+        return myAttribute;
+    }
+
+    public void setMyAttribute(Object myAttribute) {
+        this.myAttribute = myAttribute;
+    }
+
+
+}
+
