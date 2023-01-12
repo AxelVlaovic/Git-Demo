38d5020f2a6832ff05beff2a4ade8adb194e8121
AkselVlaovic <avlaovic2619ri@raf.rs>
2023-01-12 01:59:41 +0100

    PackageThree added

---
+++ src/main/java/packageThree/MyClassThree.java
@@ -0,0 +1,16 @@
+package packageThree;
+
+public class MyClassThree {
+
+    private Object myObject;
+
+    public Object getMyObject() {
+        return myObject;
+    }
+
+    public void setMyObject(Object myObject) {
+        this.myObject = myObject;
+    }
+
+
+}
