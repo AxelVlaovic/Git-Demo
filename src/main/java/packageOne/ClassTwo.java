6dfd0e59c00ef48c04ba45823875a770c36c8fee
AkselVlaovic <avlaovic2619ri@raf.rs>
2023-01-12 01:53:32 +0100

    Added packageOne

---
+++ src/main/java/packageOne/ClassTwo.java
@@ -0,0 +1,17 @@
+package packageOne;
+
+public class ClassTwo {
+
+    private Object classTwoObject;
+
+    public Object getClassTwoObject() {
+        return classTwoObject;
+    }
+
+    public void setClassTwoObject(Object classTwoObject) {
+        this.classTwoObject = classTwoObject;
+    }
+
+
+
+}
